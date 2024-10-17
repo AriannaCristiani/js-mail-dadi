6ae2b4699f24de33216e9e9b891353c53b46f358
AriannaCrisriani <ariannacristiani@yahoo.it>
2024-10-17 15:38:36 +0200

    divisione corretta in cartelle

---
+++ dadi-app.js
@@ -0,0 +1,11 @@
+//Gioco dei dadi
+//Generare un numero random da 1 a 6 per il giocatore
+//Generare un numero random da 1 a 6 per il computer
+//  SE il numero uscito al giocatore è più alto
+//      -stampa 'Congratulazioni! Hai vinto!'
+//  ALTRIMENTI SE il numero uscito al computer è più alto
+//      -stampa 'Oh no! Hai perso...ritenta un'altra volta!'
+
+
+const randomUtente = prompt ('Clicca qui per generare un numero random' (Math.random ))
+console.log( randomUtente)
